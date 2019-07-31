d28754f89e61b1942bdf80f86e13560b5c23569b
fdzrn <fadzriangibran@gmail.com>
2019-07-31 14:34:01 +0700

    Tugas 4
    tugas di kirimkan kepada Niomic

---
+++ tugas4.js
@@ -0,0 +1,13 @@
+function tinggiSiswa(tinggi) {
+	if (tinggi >= 170) {
+		console.log('Kamu Paling tinggi');
+	} else if (tinggi >= 160) {
+		console.log('Kamu Cukup tinggi');
+	} else {
+		console.log('Maaf Kamu Pendek')
+	}
+}
+
+tinggiSiswa(170);
+tinggiSiswa(165);
+tinggiSiswa(155);
